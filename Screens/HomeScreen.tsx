76813668915135a931c387cf422203ceb25e3301
Darshan Dinesh <darshandineshmp@gmail.com>
2024-07-03 14:48:30 +0530

    gave help or FAB

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View, ImageBackground } from 'react-native';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React, { useEffect, useState, useRef } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, ImageBackground, Modal, Animated, TouchableWithoutFeedback, Linking } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   const [categories, setCategories] = useState([]);
+  const [contact, setContact] = useState(false);
+  const opacity = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     async function fetchCategories() {
@@ -16,6 +20,22 @@ const HomeScreen = ({ navigation }) => {
     }
     fetchCategories();
   }, []);
+
+  useEffect(() => {
+    if (contact) {
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [contact, opacity]);
 
   return (
     <View style={styles.container}>
@@ -40,7 +60,37 @@ const HomeScreen = ({ navigation }) => {
         )}
         keyExtractor={(item) => item.id.toString()}
       />
-    </View>
+      <TouchableOpacity
+        style={styles.fab}
+        onPress={() => setContact(!contact)}
+      >
+        <FontAwesomeIcon icon={faCircleQuestion} size={24} color="#22223B" />
+      </TouchableOpacity>
+      <Modal
+        transparent={true}
+        visible={contact}
+        animationType="none"
+        onRequestClose={() => setContact(false)}
+      >
+        <TouchableWithoutFeedback onPress={() => setContact(false)}>
+          <View style={styles.modalOverlay}>
+            <TouchableWithoutFeedback>
+              <Animated.View style={[styles.modalContent, { opacity }]}>
+                <Text style={styles.modalTitle}>Hey there!</Text>
+                <Text style={styles.modalText}>Raise issue in my <Text style={{ fontWeight: '500', textDecorationLine: 'underline', color: 'darkblue' }} onPress={() => Linking.openURL('https://github.com/nu23mca28')}>GitHub</Text></Text>
+
+                <TouchableOpacity
+                  style={styles.closeButton}
+                  onPress={() => setContact(false)}
+                >
+                  <Text style={styles.closeButtonText}>Close</Text>
+                </TouchableOpacity>
+              </Animated.View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </View >
   );
 };
 
@@ -82,10 +132,58 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
+  },
+  fab: {
+    position: 'absolute',
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 20,
+    bottom: 20,
+    backgroundColor: '#EEEEF6',
+    borderRadius: 30,
+    elevation: 8,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#22223B',
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#22223B',
+  },
+  closeButton: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#22223B',
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
